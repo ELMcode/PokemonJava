a3bd88fb4d7da48fb5f6ff00ec51d16114b60dd5
Arheee <125261469+Arheee@users.noreply.github.com>
2023-12-15 09:43:24 +0100

    Refactor(PokemonApp/model) :refactor constructor :recycle:

--- src/main/java/pokemonapp/model/Pokemon.java
+++ src/main/java/pokemonapp/model/Pokemon.java
@@ -12,34 +12,34 @@ public class Pokemon extends Attaque {
 
      private Type type;
 
-    public Pokemon(String feu,
-                   String vol,
-                   String eau,
-                   String codeur,
-                   String nom,
+    public Pokemon(String nom,
+                   String nomAttaque,
+                   int puissanceMax,
+                   int puissanceMin,
+                   String nomPokemon,
                    int hpMax,
                    int hpRestant,
                    List<Attaque> attaques,
                    Type type
     ) {
         super(
-                feu,
-                vol,
-                eau,
-                codeur
-        );
-        this.nom = nom;
+                nom,
+                nomAttaque,
+                puissanceMax,
+                puissanceMin);
+        this.nom = nomPokemon;
         this.hpMax = hpMax;
         this.hpRestant = hpRestant;
         this.attaques = attaques;
         this.type = type;
     }
 
-
+    @Override
     public String getNom() {
         return nom;
     }
 
+    @Override
     public void setNom(String nom) {
         this.nom = nom;
     }
