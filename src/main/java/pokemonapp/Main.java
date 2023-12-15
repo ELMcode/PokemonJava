2c10f99fc78f98d7b05c62e42f9805c94ec96d85
amine.elalame <amine.elalame@pingflow.fr>
2023-12-15 10:31:57 +0100

    feat(pokemonapp): Create new Dresseur in Main

--- src/main/java/pokemonapp/Main.java
+++ src/main/java/pokemonapp/Main.java
@@ -1,7 +1,25 @@
 package pokemonapp;
 
+import pokemonapp.model.Dresseur;
+import pokemonapp.model.Inventaire;
+import pokemonapp.model.Pokemon;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
 
+        Dresseur Dresseur1 = new Dresseur(
+                "Sacha",
+                new Inventaire(
+                        3,
+                        6,
+                        getRandomPokemon()
+                )
+
+
+        )
+
     }
-}
+
+
