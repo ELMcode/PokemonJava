ef55352e962da8fc81b8630b8353085716ed99ff
amine.elalame <amine.elalame@pingflow.fr>
2023-12-15 10:56:25 +0100

    feat(pokemonapp): Initialize new Dresseur Object & new Inventaire Object

--- src/main/java/pokemonapp/Main.java
+++ src/main/java/pokemonapp/Main.java
@@ -7,19 +7,33 @@ import pokemonapp.model.Pokemon;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        Inventaire Inventaire1 = new Inventaire(
+                3,
+                6,
+                Dresseur1.getRandomPokemon()
+                );
+
+        Inventaire Inventaire2 = new Inventaire(
+                3,
+                6,
+                Dresseur2.getRandomPokemon()
+                );
 
         Dresseur Dresseur1 = new Dresseur(
                 "Sacha",
-                new Inventaire(
-                        3,
-                        6,
-                        getRandomPokemon()
-                )
+                Inventaire1,
+                1
+        );
 
-
-        )
+        Dresseur Dresseur2 = new Dresseur(
+                "Toto",
+                Inventaire2,
+                2
+        );
 
     }
+}
 
 
